33d77b9abc76c64a814ba07aeedf2b31737c437c
catagonus <107582977+catagonus@users.noreply.github.com>
2024-02-16 16:53:28 -0500

    Add calculator functionality - add ability to add, subtract, multiply, and divide user input - make calculator display result - make clear button clear the display - make the change sign button change the sign of last number

--- javascript.js
+++ javascript.js
@@ -28,12 +28,72 @@ function operate(firstOperand, operator, secondOperand) {
         case '-':
             return subtract(firstOperand, secondOperand);
             break;
-        case '*':
+        case 'X':
             return multiply(firstOperand, secondOperand);
             break;
-        case '/':
+        case '%':
             return divide(firstOperand, secondOperand);
             break;
     }
 }
+
+let memoryValue = '';
+let resultValue = '';
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+
+const memoryDisplay = document.querySelector('#memory');
+const displayResult = document.querySelector('#result');
+
+numbers.forEach(button => {
+    button.addEventListener('click', e => {
+        memoryValue += e.target.textContent;
+        memoryDisplay.textContent = memoryValue;
+    });
+});
+
+let operatorsCount = 0;
+
+operators.forEach(button => {
+    button.addEventListener('click', e => {
+        operatorsCount++;
+        if ( operatorsCount > 1 ) {
+            calculateResult();
+        }
+        memoryValue += ' ' + e.target.textContent + ' ';
+        memoryDisplay.textContent = memoryValue;
+    });
+});
+
+const clearButton = document.querySelector('#clear');
+
+clearButton.addEventListener('click', () => {
+    memoryDisplay.textContent = '';
+    displayResult.textContent = '';
+    memoryValue = '';
+    operatorsCount = 0;
+});
+
+const equalButton = document.querySelector('#equals');
+
+equalButton.addEventListener('click', calculateResult);
+
+function calculateResult() {
+    const memoryArray = memoryValue.toString().split(' ');
+    console.log(memoryArray);
+    memoryValue = operate(memoryArray[0], memoryArray[1], memoryArray[2]);
+    resultValue = memoryValue;
+    displayResult.textContent = resultValue;
+    operatorsCount = 0;
+}
+
+const signChangeButton = document.querySelector('#changeSign');
+
+signChangeButton.addEventListener('click', () => {
+    const memoryArray = memoryValue.toString().split(' ');
+    memoryArray[memoryArray.length-1] *= -1;
+    memoryValue = memoryArray.join(' ');
+});
+
 
